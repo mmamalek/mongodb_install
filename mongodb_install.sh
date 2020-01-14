98f6035bf92570c0e66f577dc2e052020e28beb5
mmamalek <50703300+mmamalek@users.noreply.github.com>
2020-01-14 13:02:17 +0200

    fix the 'dir not found' bug
    Can now run the shell from any directory to install mongodb

--- mongodb_install.sh
+++ mongodb_install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 curl -o ~/mongodb.tgz https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-4.2.2.tgz
 tar -zxvf ~/mongodb.tgz
-rm -f mongodb.tgz
-mv mongodb-macos-x86_64-4.2.2 mongodb
+rm -f ~/mongodb.tgz
+mv ~/mongodb-macos-x86_64-4.2.2 ~/mongodb
 
 mkdir ~/mongodb/db
 mkdir ~/mongodb/logs
 
 echo "\nexport PATH=\$PATH:\$HOME/mongodb/bin\n" >> ~/.zshrc
-echo "\nalias mongod='mongod --dbpath ~/mongodb/db --logpath ~/mongodb/logs/mongo.log'" >> ~/.zshrc
+echo "\nalias mongod='mongod --dbpath ~/mongodb/db --logpath ~/mongodb/logs/mongo.log --fork'" >> ~/.zshrc
 
 source ~/.zshrc
